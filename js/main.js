3f11cac885c297132ab994d26b2f3a53b7a77c93
Fastroof <LytvDan@gmail.com>
2021-06-03 16:23:16 +0300

    upd 03.06.2021

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// var images = ["resources/slider/slider-1.jpg", "resources/slider/slider-2.jpg", "resources/slider/slider-3.jpg"]
+// var index = 0;
+//
+// function change() {
+//   if (index == 2) {
+//     index = 0;
+//   } else {
+//     index++;
+//   }
+//   if (index == 0) {
+//     document.getElementsByClassName("hello")[0].style.backgroundImage = "url('resources/slider/slider-1.jpg'), url('resources/slider/slider-2.jpg');";
+//   } else if (index == 1) {
+//     document.getElementsByClassName("hello")[0].style.backgroundImage = "url('resources/slider/slider-2.jpg'), url('resources/slider/slider-3.jpg');";
+//   } else {
+//     document.getElementsByClassName("hello")[0].style.backgroundImage = "url('resources/slider/slider-3.jpg'), url('resources/slider/slider-1.jpg');";
+//   }
+//
+// }
+//
+// window.onload = function () {
+//   setInterval(change, 1000);
+// };
